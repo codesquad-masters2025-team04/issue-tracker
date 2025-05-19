198d59b54c1a001a789873c5ad3c51bcce9010e8
jicho <jIcho6412@gmail.com>
2025-05-19 11:23:33 +0900

    feat: 이슈 작성, 이슈 목록 페이지에서 필터링 기능 구현
    - 이슈 작성 페이지에서 옵션 클릭 후 완료 버튼 누르면 해당 옵션들이 적용된 상태로 전달
    - 이슈 목록 페이지에서 필터링 UI 구현

--- frontend/src/components/Dashboard/TableHeader.jsx
+++ frontend/src/components/Dashboard/TableHeader.jsx
@@ -13,10 +13,42 @@ const changeFilterName = {
 // TODO 추후 컴포넌트 분리 예정
 function TableHeader({ isOpen, setIsOpen, issueCount, filterData }) {
   const [activeFilter, setActiveFilter] = useState(null);
+  const [selectedFilters, setSelectedFilters] = useState({
+    담당자: [],
+    레이블: [],
+    마일스톤: null,
+    작성자: [], // 작성자도 추가
+  });
 
   const handleFilterClick = (filterName) => {
     setActiveFilter((prev) => (prev === filterName ? null : filterName));
   };
+
+  const handleOptionSelect = (filter, item) => {
+    setSelectedFilters((prev) => {
+      if (filter === "마일스톤") {
+        return {
+          ...prev,
+          [filter]: prev[filter]?.id === item.id ? null : item,
+        };
+      }
+
+      const alreadySelected = prev[filter].some((el) => el.id === item.id);
+      if (alreadySelected) {
+        return {
+          ...prev,
+          [filter]: prev[filter].filter((el) => el.id !== item.id),
+        };
+      }
+
+      return {
+        ...prev,
+        [filter]: [...prev[filter], item],
+      };
+    });
+    // TODO 추후 setActiveFilter(null); 을 넣을지 말지 고민해보기
+  };
+
   return (
     <div className={styles.tableHeader}>
       <div className={styles.issueViewControls}>
@@ -50,6 +82,8 @@ function TableHeader({ isOpen, setIsOpen, issueCount, filterData }) {
                 filterName={name}
                 actionLocation={"filterBar"}
                 data={filterData?.[changeFilterName[name]] ?? []}
+                onSelect={(item) => handleOptionSelect(name, item)}
+                selectedItems={selectedFilters[name]}
               />
             )}
           </div>
