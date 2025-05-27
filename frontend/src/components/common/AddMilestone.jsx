238cd9cbe31f26a05c9e3ea47a6058bc5b14210d
jicho <jIcho6412@gmail.com>
2025-05-27 23:27:55 +0900

    fix: 문자열 내에 있는 모든 공백을 제거하기 위해 trim()대신 replace 사용하여 처리

--- frontend/src/components/common/AddMilestone.jsx
+++ frontend/src/components/common/AddMilestone.jsx
@@ -34,8 +34,8 @@ function AddMilestone({
   };
 
   const isVaildDate = (dateString) => {
-    const trimmedDate = dateString.trim();
-    const regex = /^\d{4}\. (0[1-9]|1[0-2])\. (0[1-9]|[12][0-9]|3[01])$/;
+    const trimmedDate = dateString.replace(/\s/g, "");
+    const regex = /^\d{4}\.(0[1-9]|1[0-2])\.(0[1-9]|[12][0-9]|3[01])$/;
     if (!regex.test(trimmedDate)) return false;
 
     const [year, month, day] = trimmedDate.split(".").map(Number);
